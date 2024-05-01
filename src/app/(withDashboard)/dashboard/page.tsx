5a97ab7ba88fe0c0a36ff4d136c8ee7e701633dc
JAHID HASAN <developerjahid@gmail.com>
2024-05-01 19:36:06 +0600

    logout function added to navbar

--- src/app/(withDashboard)/dashboard/page.tsx
+++ src/app/(withDashboard)/dashboard/page.tsx
@@ -12,7 +12,7 @@ const DashboardPage = async() => {
         <h1 className="text-4xl text-center mt-10">Welcome {session?.user?.name}</h1>
         <h1 className="text-4xl text-center mt-10">Welcome {session?.user?.email}</h1>
         <Image
-         src={session?.user?.image }
+         src={session?.user?.image ||'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png' }
          width ={500}
          height={500}
          alt="user iamge"
